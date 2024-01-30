a5b8ed07b0d559a8c88835520187a7a6c1a14b33
AlxsG <alxsg74@gmail.com>
2024-01-30 11:42:04 -0600

    Fixed issue with camera permission, Alert was not added properly

--- components/ImagePicker/ImagePicker.js
+++ components/ImagePicker/ImagePicker.js
@@ -1,4 +1,4 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Alert, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
